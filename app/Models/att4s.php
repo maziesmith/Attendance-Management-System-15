bcf36b0f1ce2bd6adde2434647f700c3e5538c06
Thusha <30925366+Thushacsc@users.noreply.github.com>
2019-08-04 10:44:12 +0530

    Added Start_Time, End_Time, date and hall for all, Calculated and stored hours and Viewed those things in view part for all

--- app/Models/att4s.php
+++ app/Models/att4s.php
@@ -18,13 +18,20 @@ class att4s extends Model
                   'name',
                   'nooflectures',
                   'lectureattend',
+                  'Start_Time',
+                  'End_Time',
                   'hours',
-            
+                  'hall',
+                  'date',
                   'attend_mark',
                   'precentage',
                   'nooflectures1',
                   'lectureattend1',
+                  'Start_Time1',
+                  'End_Time1',
                   'hours1',
+                  'hall1',
+                  'date1',
                   'attend_mark1',
                   'precentage1'
                   
@@ -35,20 +42,6 @@ class att4s extends Model
     
   
     protected $casts = [];
-    
-     public function setDateAttribute($value)
-    {
-        $this->attributes['date'] = !empty($value) ? \DateTime::createFromFormat($this->getDateFormat(), $value) : null;
-    }
-
-    
-    public function getCreatedAtAttribute($value)
-    {
-        return \DateTime::createFromFormat('j/n/Y g:i A', $value);
-
-    }
-    
-
  
   
 }
